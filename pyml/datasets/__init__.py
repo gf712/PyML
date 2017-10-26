084e03061c85df2586ba87269a83b2aea2a4c17c
Gil <g.f.hoben@gmail.com>
2017-10-26 14:26:36 +0100

    random data generator

---
+++ pyml/datasets/__init__.py
@@ -0,0 +1,1 @@
+from .random_data import gaussian
---
+++ pyml/datasets/random_data.py
@@ -0,0 +1,27 @@
+import random
+import time
+
+
+def gaussian(n=100, d=2, labels=3, sigma=1, seed=None, shuffle=True):
+    if seed is None:
+        seed = time.time()
+
+    # set random seed
+    random.seed(seed)
+
+    means = [[random.random() for dim in range(d)] for label in range(labels)]
+
+    datapoints = list()
+    data_labels = list()
+
+    for label, mean in zip(range(labels), means):
+        datapoints += [[random.gauss(mu=mean_i, sigma=sigma) for mean_i in mean] for x in range(n)]
+        labels_i = [label] * n
+        data_labels += labels_i
+
+    if shuffle:
+        data = list(zip(datapoints, data_labels))
+        random.shuffle(data)
+        datapoints, data_labels = zip(*data)
+
+    return datapoints, data_labels
