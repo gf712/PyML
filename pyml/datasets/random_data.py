fc4f1a8613fd53dd65bc69561e9e658280bb6699
Gil <g.f.hoben@gmail.com>
2017-10-30 10:53:07 +0000

    regression data generator

--- pyml/datasets/random_data.py
+++ pyml/datasets/random_data.py
@@ -22,3 +22,26 @@ def gaussian(n=100, d=2, labels=3, sigma=1, seed=None, shuffle=True):
         datapoints, data_labels = zip(*data)
 
     return datapoints, data_labels
+
+
+def regression(n=100, noise='gaussian', mu=0, sigma=1, x_min=[0], x_max=[10], intercept=0, gradient=[1], seed=None):
+
+    set_seed(seed=seed)
+
+    X = []
+
+    for x_min_i, x_max_i in zip(x_min, x_max):
+        X.append([x / x_max_i + random.random() for x in range(x_min_i, n)])
+
+    y = list()
+    for i in range(n):
+        y_i = sum([x[i] * gradient_i for x, gradient_i in zip(X, gradient)])
+
+        if noise == 'gaussian':
+            e = random.gauss(mu=mu, sigma=sigma)
+
+        y.append(y_i + e)
+
+    X = [[x[i] for x in X] for i in range(n)]
+
+    return X, y
