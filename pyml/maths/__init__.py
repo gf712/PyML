b73e3d7a43094ce6ae7418146a1ecae3d85eaa4b
Gil <g.f.hoben@gmail.com>
2017-11-08 09:33:40 +0000

    import least_squares in __init__.py

--- pyml/maths/__init__.py
+++ pyml/maths/__init__.py
@@ -1,2 +1,3 @@
-from .linear_algebra import dot_product, transpose, mean_squared_error, mean_absolute_error, subtract, power, divide
+from .linear_algebra import dot_product, transpose, mean_squared_error, mean_absolute_error, subtract, power, divide, \
+    least_squares
 from .math_utils import mean, max_occurence
