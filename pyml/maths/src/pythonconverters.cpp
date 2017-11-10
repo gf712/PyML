010d08bdfd6cac56a6ce9a228b03ea54e23c7711
Gil <g.f.hoben@gmail.com>
2017-11-10 16:00:04 +0000

    Auto stash before revert of "introducting flat representations of 2D arrays"

--- pyml/maths/src/pythonconverters.cpp
+++ pyml/maths/src/pythonconverters.cpp
@@ -86,44 +86,6 @@ PyObject* Convert_2DArray(double** array, int rows, int cols) {
 }
 
 
-PyObject* ConvertFlat2DArray_2DPy(double* array, int rows, int cols) {
-
-    // converts a 1D C++ array representation of a 2D array to a python list of lists
-
-    PyObject* twoDResult;
-    PyObject* row;
-    PyObject* item;
-
-    // internal representation of the array
-    int n = 0;
-
-    twoDResult = PyList_New(rows);
-
-    if (twoDResult != nullptr) {
-
-        for (int j = 0; j < rows; ++j) {
-            row = PyList_New(cols);
-
-            if (row != nullptr) {
-
-                for (int k = 0; k < cols; ++k) {
-                    item = PyFloat_FromDouble(array[n]);
-                    PyList_SET_ITEM(row, k, item);
-                    n++;
-                }
-
-                PyList_SET_ITEM(twoDResult, j, row);
-            }
-
-
-        }
-
-    }
-
-    return twoDResult;
-}
-
-
 void convertPy_1DArray(PyObject* array, double* result, int size) {
 
     // converts a python list to a C++ 1D array
@@ -162,38 +124,5 @@ void convertPy_2DArray(PyObject* array, double** result, int rows, int cols) {
             result[i][j] = PyFloat_AsDouble(PyList_GET_ITEM(row, j));
         }
 
-    }
-}
-
-
-void convertPy2D_flat2DArray(PyObject *array, double *result, int rows, int cols) {
-
-    // converts a python list of lists to a C++ 1D array representation of a 2D array
-
-    // row is a python object that will point to the current row
-
-    PyObject* row;
-    // n is the position in the flat matrix
-    int n = 0;
-
-    // iterate through python list and populate C++ array
-    for (int i = 0; i < rows; ++i) {
-
-        row = PyList_GET_ITEM(array, i);
-
-        if (cols != PyList_GET_SIZE(row)) {
-            std::string error1 = "Size of row ";
-            std::string error2 = " is ";
-            std::string error3 = " but expected row of size ";
-            int length = PyList_GET_SIZE(row);
-            std::string resultE;
-            resultE = error1 + std::to_string(i) + error2 + std::to_string(length) + error3 + std::to_string(cols);
-            PyErr_SetString(PyExc_ValueError, resultE.c_str());
-        }
-
-        for (int j = 0; j < cols; ++j) {
-            result[n] = PyFloat_AsDouble(PyList_GET_ITEM(row, j));
-            n++;
-        }
     }
 }
