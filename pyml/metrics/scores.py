4d9b7ee41b3315bd5baf871c62f271c0364c2010
Gil <g.f.hoben@gmail.com>
2017-11-15 15:01:29 +0000

    accuracy calc

--- pyml/metrics/scores.py
+++ pyml/metrics/scores.py
@@ -7,3 +7,7 @@ def mean_squared_error(y, y_true):
 
 def mean_absolute_error(y, y_true):
     return mean([abs(y_i - y_true_i) for y_i, y_true_i in zip(y, y_true)])
+
+
+def accuracy(y, y_true):
+    return sum([1 for y_i, y_true_i in zip(y, y_true) if y_i == y_true_i]) / len(y)
