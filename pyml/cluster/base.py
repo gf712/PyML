92045bdb4cb75b3b6ab72555c399f2a4bdc2cffe
Gil <g.f.hoben@gmail.com>
2017-11-17 14:58:33 +0000

    cluster documentation

--- pyml/cluster/base.py
+++ pyml/cluster/base.py
@@ -1,23 +1,39 @@
 from ..base import BaseLearner
-from ..maths import mean, transpose, argsort
+from ..maths import mean, argsort
 from ..metrics.distances import calculate_distance
 import random
 
 
 class ClusterBase(BaseLearner):
 
+    """
+    Base class for clustering.
+    """
+
     def __init__(self):
         BaseLearner.__init__(self)
 
     def _get_cluster(self, cluster_label=0):
         """
-        method to retrieve the index of all data points in a cluster
-        :return: list
+        Method to retrieve the index of all data points in a cluster.
+        Returns a list containing indices of datapoints belonging to cluster
+
+        :param cluster_label: cluster label
+        :type cluster_label: int
+        :rtype: list
+        :return: indices of datapoints belonging to cluster
         """
         return [i for i, label in enumerate(self._labels) if label == cluster_label]
 
     def _initialise_centroids(self):
+        """
+        Method to initialise centroids.
+        Currently supports Forgy and Random initialisation
+        (see https://en.wikipedia.org/wiki/K-means_clustering#Initialization_methods)
+        This method directly sets centroids with the self._centroids attribute of the child class.
 
+        :rtype: None
+        """
         if self._initialisation == 'Forgy':
             # choose k random datapoints to be centroids
             indices = set()
@@ -33,9 +49,13 @@ class ClusterBase(BaseLearner):
 
     def _get_cluster_mean(self, cluster_label=0):
         """
-        Calculate centroid location
-        :param cluster_label:
-        :return: list
+        Calculate centroid location (which is the mean in each dimension of all points in a cluster).
+        Returns a list with M coordinates for the cluster_label centroid.
+
+        :param cluster_label: cluster number
+        :type cluster_label: int
+        :rtype: list
+        :return: mean value of each dimension of cluster_label
         """
 
         datapoints_index = self._get_cluster(cluster_label=cluster_label)
@@ -43,10 +63,17 @@ class ClusterBase(BaseLearner):
         # return the average of each dimension
         return mean(datapoints, axis=0)
 
-    def _update_centroids(self):
-        self._centroids = [self._get_cluster_mean(x) for x in range(self.k)]
-
     def _assign_cluster(self, X):
+        """
+        Cluster assignment given the norm in the child class.
+        Returns a list with the cluster labels (of size len(X))
+
+        :param X: datapoints to be classified
+        :type X: list of lists
+        :rtype: list
+        :return: cluster labels
+        """
+
         # calculate distance to each centroid
         distances = [calculate_distance(X, self._centroids[i], self.norm) for i in range(self.k)]
         # distances_T = transpose(distances)
@@ -55,6 +82,11 @@ class ClusterBase(BaseLearner):
         return [x[0] for x in argsort(distances, axis=0)]
 
     def _changes(self):
+        """
+        Keeps track of the number of changes per iteration.
+        :return: number of changes per iteration
+        :rtype: int
+        """
         change_n = 0
         for cluster in range(self.k):
             indices_set = set(self._get_cluster(cluster_label=cluster))
