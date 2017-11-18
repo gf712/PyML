1455d75bc3fa7a24aa04eff9914c61d4ce8386cd
Gil <g.f.hoben@gmail.com>
2017-11-18 22:24:25 +0000

    fixed argmin and argmax

--- pyml/maths/src/mathsextension.cpp
+++ pyml/maths/src/mathsextension.cpp
@@ -118,6 +118,190 @@ static PyObject* quick_sort(PyObject* self, PyObject *args) {
 }
 
 
+static PyObject* Cargmax(PyObject* self, PyObject *args) {
+
+    // variable instantiation
+    auto A = new flatArray;
+    auto resultList = new flatArray;
+    int axis;
+
+    // pointers to python lists
+    PyObject* pA;
+
+    // return error if we don't get all the arguments
+    if (!PyArg_ParseTuple(args, "O!i", &PyList_Type, &pA, &axis)) {
+        PyErr_SetString(PyExc_TypeError, "Expected one lists and an integer!");
+        return nullptr;
+    }
+
+    // read in Python list
+    A->readFromPythonList(pA);
+
+    if (A->getRows() == 1) {
+
+        // if A is a vector
+        double *array = A->getRow(0);
+
+        resultList->startEmptyArray(1, 1);
+
+        resultList->setNElement(argmax(array, A->getCols()), 0);
+
+        delete [] array;
+    }
+
+    else {
+        // if A is a matrix
+        if (axis == 1) {
+
+            resultList->startEmptyArray(1, A->getRows());
+
+            double *array = nullptr;
+
+            // if axis is 1 return row wise argmax
+            for (int i = 0; i < A->getRows(); ++i) {
+
+                array = A->getRow(i);
+
+                resultList->setNElement(argmax(array, A->getCols()), i);
+
+            }
+
+            delete [] array;
+
+        }
+        else if (axis == 0) {
+
+            resultList->startEmptyArray(1, A->getCols());
+
+            double *array = nullptr;
+
+            // if axis is 1 return column wise argmax
+            for (int i = 0; i < A->getCols(); ++i) {
+
+                array = A->getCol(i);
+
+                resultList->setNElement(argmax(array, A->getRows()), i);
+
+            }
+
+            delete [] array;
+
+        }
+        else {
+            // ERROR
+            PyErr_SetString(PyExc_TypeError, "Expected axis value to be 0 or 1");
+            return nullptr;
+        }
+    }
+
+
+    // convert result to python list
+    PyObject* result_py_list = ConvertFlatArray_PyList(resultList, "int");
+
+    // build python object
+    PyObject *FinalResult = Py_BuildValue("O", result_py_list);
+
+    // free up memory
+    delete A;
+
+    Py_DECREF(result_py_list);
+
+    return FinalResult;
+}
+
+
+static PyObject* Cargmin(PyObject* self, PyObject *args) {
+
+    // variable instantiation
+    auto A = new flatArray;
+    auto resultList = new flatArray;
+    int axis;
+
+    // pointers to python lists
+    PyObject* pA;
+
+    // return error if we don't get all the arguments
+    if (!PyArg_ParseTuple(args, "O!i", &PyList_Type, &pA, &axis)) {
+        PyErr_SetString(PyExc_TypeError, "Expected one lists and an integer!");
+        return nullptr;
+    }
+
+    // read in Python list
+    A->readFromPythonList(pA);
+
+    if (A->getRows() == 1) {
+
+        // if A is a vector
+        double *array = A->getRow(0);
+
+        resultList->startEmptyArray(1, 1);
+
+        resultList->setNElement(argmin(array, A->getCols()), 0);
+
+        delete [] array;
+    }
+
+    else {
+        // if A is a matrix
+        if (axis == 1) {
+
+            resultList->startEmptyArray(1, A->getRows());
+
+            double *array = nullptr;
+
+            // if axis is 1 return row wise argmax
+            for (int i = 0; i < A->getRows(); ++i) {
+
+                array = A->getRow(i);
+
+                resultList->setNElement(argmin(array, A->getCols()), i);
+
+            }
+
+            delete [] array;
+
+        }
+        else if (axis == 0) {
+
+            resultList->startEmptyArray(1, A->getCols());
+
+            double *array = nullptr;
+
+            // if axis is 1 return column wise argmax
+            for (int i = 0; i < A->getCols(); ++i) {
+
+                array = A->getCol(i);
+
+                resultList->setNElement(argmin(array, A->getRows()), i);
+
+            }
+
+            delete [] array;
+
+        }
+        else {
+            // ERROR
+            PyErr_SetString(PyExc_TypeError, "Expected axis value to be 0 or 1");
+            return nullptr;
+        }
+    }
+
+
+    // convert result to python list
+    PyObject* result_py_list = ConvertFlatArray_PyList(resultList, "int");
+
+    // build python object
+    PyObject *FinalResult = Py_BuildValue("O", result_py_list);
+
+    // free up memory
+    delete A;
+
+    Py_DECREF(result_py_list);
+
+    return FinalResult;
+}
+
+
 static PyObject* version(PyObject* self) {
     return Py_BuildValue("s", "Version 0.1");
 }
@@ -126,6 +310,8 @@ static PyObject* version(PyObject* self) {
 static PyMethodDef CMathsMethods[] = {
         // Python name    C function              argument representation  description
         {"quick_sort",    quick_sort,             METH_VARARGS,            "Quick sort algorithm"},
+        {"Cargmin",       Cargmin,                METH_VARARGS,            "Returns index of minimum value"},
+        {"Cargmax",       Cargmax,                METH_VARARGS,            "Returns index of maximum value"},
         {"version",       (PyCFunction)version,   METH_NOARGS,             "Returns version."},
         {nullptr, nullptr, 0, nullptr}
 };
