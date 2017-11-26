14184b9b502a3e39ddfd903078d39795477285a1
Gil <g.f.hoben@gmail.com>
2017-11-26 21:42:00 +0000

    more regression tests with mini batch GD

--- tests/regression_tests.py
+++ tests/regression_tests.py
@@ -136,41 +136,6 @@ class MultiClassLogisticRegressionTest(unittest.TestCase):
         self.assertEqual(self.classifier.seed, 1970)
 
 
-class LogisticRegressionTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.X, cls.y = X, y = gaussian(labels=2, sigma=0.2, seed=1970)
-        cls.X_train, cls.y_train, cls.X_test, cls.y_test = train_test_split(cls.X, cls.y,
-                                                                            train_split=0.8, seed=1970)
-        cls.classifier = LogisticRegression(seed=1970)
-        cls.classifier.train(X=cls.X_train, y=cls.y_train)
-
-    def test_log_iterations(self):
-        self.assertEqual(self.classifier.iterations, 1623)
-
-    def test_log_coefficients(self):
-        self.assertAlmostEqual(self.classifier.coefficients[0], -1.1576475345638408, delta=0.001)
-        self.assertAlmostEqual(self.classifier.coefficients[1], 0.1437129269620468, delta=0.001)
-        self.assertAlmostEqual(self.classifier.coefficients[2], 2.4464052394504856, delta=0.001)
-
-    def test_log_cost(self):
-        self.assertAlmostEqual(self.classifier.cost[0], -106.11158912690777, delta=0.001)
-        self.assertAlmostEqual(self.classifier.cost[-1], -61.16035391042087, delta=0.001)
-
-    def test_log_predict(self):
-        self.assertEqual(self.classifier.predict(self.X_test)[0], 1)
-
-    def test_log_predict_proba(self):
-        self.assertAlmostEqual(self.classifier.predict_proba(self.X_test)[0], 0.807766417948826, delta=0.001)
-
-    def test_log_accuracy(self):
-        self.assertAlmostEqual(self.classifier.score(self.X_test, self.y_test), 0.975, delta=0.001)
-
-    def test_log_seed(self):
-        self.assertEqual(self.classifier.seed, 1970)
-
-
 class MultiClassLogisticRegressionwithMomentumTest(unittest.TestCase):
 
     @classmethod
@@ -201,6 +166,39 @@ class MultiClassLogisticRegressionwithMomentumTest(unittest.TestCase):
 
     def test_log_predict_proba(self):
         self.assertAlmostEqual(self.classifier.predict_proba(self.X_test)[0][0], 0.04680865754859053, delta=0.001)
+
+    def test_log_accuracy(self):
+        self.assertAlmostEqual(self.classifier.score(self.X_test, self.y_test), 0.9833333333333333, delta=0.001)
+
+
+class MultiClassLogisticRegressionMiniBatch(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.X, cls.y = gaussian(labels=3, sigma=0.2, seed=1970)
+        cls.X_train, cls.y_train, cls.X_test, cls.y_test = train_test_split(cls .X, cls.y,
+                                                                            train_split=0.8, seed=1970)
+        cls.classifier = LogisticRegression(seed=1970, alpha=0.9, batch_size=64)
+        cls.classifier.train(X=cls.X_train, y=cls.y_train)
+
+    def test_log_iterations(self):
+        self.assertEqual(self.classifier.iterations[0], 1670)
+
+    def test_log_coefficients(self):
+        self.assertAlmostEqual(self.classifier.coefficients[0][-1], -6.166939294187147, delta=0.001)
+        self.assertAlmostEqual(self.classifier.coefficients[1][-1], 3.9011202616018834, delta=0.001)
+        self.assertAlmostEqual(self.classifier.coefficients[2][-1], 1.4360170566346704, delta=0.001)
+
+    def test_log_cost(self):
+        self.assertAlmostEqual(self.classifier.cost[0][-1], -38.882733287747826, delta=0.001)
+        self.assertAlmostEqual(self.classifier.cost[1][-1], -71.22041849047173, delta=0.001)
+        self.assertAlmostEqual(self.classifier.cost[2][-1], -39.508909436219405, delta=0.001)
+
+    def test_log_predict(self):
+        self.assertEqual(self.classifier.predict(self.X_test)[0], 1)
+
+    def test_log_predict_proba(self):
+        self.assertAlmostEqual(self.classifier.predict_proba(self.X_test)[0][0], 0.047083994898380166, delta=0.001)
 
     def test_log_accuracy(self):
         self.assertAlmostEqual(self.classifier.score(self.X_test, self.y_test), 0.9833333333333333, delta=0.001)
