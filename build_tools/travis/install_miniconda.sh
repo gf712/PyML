0c11fc23f40c2472b5e38187a16f51dda1337274
Gil <g.f.hoben@gmail.com>
2017-12-14 16:20:45 +0000

    gcc 4.9

--- build_tools/travis/install_miniconda.sh
+++ build_tools/travis/install_miniconda.sh
@@ -16,8 +16,8 @@ fi
 if [[ ${TRAVIS_OS_NAME} = osx ]]; then
     # Since default gcc on osx is just a front-end for LLVM
     if [[ ${CC} = gcc ]]; then
-        export CXX=g++-4.8
-        export CC=gcc-4.8
+        export CXX=g++-4.9
+        export CC=gcc-4.9
     fi
 fi
 
