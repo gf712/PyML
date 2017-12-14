f730209aa60ead78754fe702e4fb5e32254fca44
Gil <g.f.hoben@gmail.com>
2017-12-14 14:48:21 +0000

    use gcc compiler rather than clang

--- build_tools/travis/install_miniconda.sh
+++ build_tools/travis/install_miniconda.sh
@@ -13,6 +13,15 @@ if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
 fi
 
 
+if [[ ${TRAVIS_OS_NAME} = osx ]]; then
+    # Since default gcc on osx is just a front-end for LLVM
+    if [[ ${CC} = gcc ]]; then
+        export CXX=g++-4.8
+        export CC=gcc-4.8
+    fi
+fi
+
+
 bash $MINICONDA -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
