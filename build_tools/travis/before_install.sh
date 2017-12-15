21cf0a4af783ca61594a59bcd2fef3ba03e150ff
Gil <g.f.hoben@gmail.com>
2017-12-15 09:55:29 +0000

    fixed bash script

--- build_tools/travis/before_install.sh
+++ build_tools/travis/before_install.sh
@@ -4,9 +4,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then sudo apt-get update; fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update && brew install md5sha1sum; fi
 
-if [[ ${TRAVIS_OS_NAME} = "osx" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 # Since default gcc on osx is just a front-end for LLVM
-    if [[ ${CC} = "gcc" ]]; then
+    if [[ "$CC" == "gcc" ]]; then
         export CXX=g++-4.9
         export CC=gcc-4.9
     fi
