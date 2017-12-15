23a5a8f6c64172634889bc8758f09cc14e09f467
Gil <g.f.hoben@gmail.com>
2017-12-15 11:25:57 +0000

    compile with clang

--- build_tools/travis/before_install.sh
+++ build_tools/travis/before_install.sh
@@ -4,10 +4,10 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then apt-get update; fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update && brew install md5sha1sum; fi
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-# Since default gcc on osx is just a front-end for LLVM
-    if [[ "$CC" == "gcc" ]]; then
-        export CXX=g++-4.9
-        export CC=gcc-4.9
-    fi
-fi
+#if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+## Since default gcc on osx is just a front-end for LLVM
+#    if [[ "$CC" == "gcc" ]]; then
+#        export CXX=g++-4.8
+#        export CC=gcc-4.8
+#    fi
+#fi
